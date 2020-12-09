0da07771dfd72c2bbd0ebba635a2c85456611b6b
Jay <34258275+sell@users.noreply.github.com>
2020-12-09 00:51:04 -0500

    :bug:

--- client/src/views/Notifications.vue
+++ client/src/views/Notifications.vue
@@ -132,6 +132,7 @@ export default {
         this.message = data.message;
       } catch (e) {
         this.loading = false;
+        console.log(e);
         this.error = e.response && e.response.error || 'Something happened, try again later.';
       }
     },
